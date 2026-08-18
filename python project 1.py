45e1e8916b50ef8a3c6eeb0dd6f43161f695a100
Armin Khareghat <amrawat0110@gmail.com>
2026-08-18 08:58:17 +0530

    Fix input function type and clean up output

--- python project 1.py
+++ python project 1.py
@@ -23,22 +23,3 @@ print("Your rounded height is :", rounded_height)
 print("Thank you for using the personal data collector. Goodbye!")
 
 
-Output:
-Welcome to the Interactive Personal Data Collector!
-
-Please enter your name: Armin
-Please enter your age: 23
-Please enter your height in meters: 1.75
-Please enter your favorite number: 10
-
-Thank you! Here is the information we have collected:
-
-Name: Armin (Type: <class 'str'> , Memory Address: 140123456789456 )
-Age: 20 (Type: <class 'int'> , Memory Address: 140123456780112 )
-Height: 1.75 (Type: <class 'float'> , Memory Address: 140123456781904 )
-Favorite Number: 7 (Type: <class 'int'> , Memory Address: 140123456779888 )
-
-Your birth year is approximately: 2003 (based on your age of 23)
-Your rounded height is: 1
-
-Thank you for using the Personal Data Collector. Goodbye!
